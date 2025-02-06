dac3e915de5ef3290e48de7a66abed8868bd0533
adeliba124 <adeliba124@g.educaand.es>
2025-02-06 12:53:11 +0100

    proyecto

--- Proyecto/Color-flipper/js/script_hex.js
+++
@@ -1,8 +0,0 @@
-const btn = document.getElementById('btn');
-const colorCode = document.getElementById('color-code');
-
-btn.addEventListener('click', () => {
-    const randomHex = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
-    document.body.style.backgroundColor = randomHex;
-    colorCode.textContent = `Background color: ${randomHex}`;
-});
